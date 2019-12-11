8d51885d05ab854f10379c502afbffd77fd73b4f
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-12-11 18:25:59 +0100

    deleted the pari

--- app/models/pari.rb
+++
@@ -1,2 +0,0 @@
-class Pari < ApplicationRecord
-end
--- db/migrate/20191201161205_create_paris.rb
+++
@@ -1,9 +0,0 @@
-class CreateParis < ActiveRecord::Migration[5.2]
-  def change
-    create_table :paris do |t|
-      t.string :arrondissement
-
-      t.timestamps
-    end
-  end
-end
