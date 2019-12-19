c476011dfc180604839f1f44e638dd5188aba9d0
John Jomard <john-mackenzie.jomard@mail.mcgill.ca>
2019-12-19 23:06:04 +0100

    t

---
+++ app/javascript/components/modal.js
@@ -0,0 +1,30 @@
+let modalBtn   = document.getElementById("modal-btn")
+let modal      = document.querySelector(".modal")
+let signup_btn = document.querySelector(".signup-btn")
+let login_btn  = document.querySelector(".login-btn")
+let signup     = document.querySelector(".signup")
+let login      = document.querySelector(".login")
+
+signup_btn.onclick = function(){
+  signup.classList.remove("hidden");
+  login.classList.add("hidden");
+  signup_btn.classList.remove("btn-opacity");
+  login_btn.classList.add("btn-opacity");
+}
+
+login_btn.onclick = function(){
+  login.classList.remove("hidden");
+  signup.classList.add("hidden");
+  signup_btn.classList.add("btn-opacity");
+  login_btn.classList.remove("btn-opacity");
+}
+
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
